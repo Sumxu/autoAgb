b625c5d96640e6e6d94ffe1a4c9a2f3e7dbcce22
2055185134 <2055185134@qq.com>
2025-12-15 20:25:06 +0800

    完成版本

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+const config = {
+    days:0, //0/1/2 表示 1天/15天/30天  //下拉框
+   minAmount:200, //最小投入金额
+   maxAmount:300,//最大投入金额
+   type:0,//0 间隔多少秒 1每分钟的多少秒
+   sec:180, //
+   initInviter:'', //绑定邀请人
+   wallets:[''],//私钥地址列表
+};
+export default config;
